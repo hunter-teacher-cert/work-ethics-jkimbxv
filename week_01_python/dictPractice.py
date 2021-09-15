c577aad1b0cfec2386f99b77c414113e59988e12
Jiyoon Kim <jkim@bronxvilleschool.org>
2021-09-14 22:45:36 -0400

    pseudocoded dictPractice.py

--- week_01_python/dictPractice.py
+++ week_01_python/dictPractice.py
@@ -1,2 +1,15 @@
 #For dictionaries you can write a program that reads a file and uses a
 #dictionary to count the number of times each word appears.
+f = open("demofile.txt","r") #f is a string of the text file inserted
+
+fileWords = {}
+
+#write a function that detects next words by using spaces.
+
+#go through and check that word is unique, loop
+
+#go through document and detect next word
+#check if appeared before. if not
+#add next word to dictionary by using syntax shown
+#fileWords[wordFromF] = 1
+#if old word, incremenent value by 1. 
